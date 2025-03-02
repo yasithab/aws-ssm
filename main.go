36055b6dfc83dffaec18223b4f0825f51b0730b5
Yasitha Bogamuwa <yasithab@gmail.com>
2025-03-03 01:36:35 +0400

    [homebrew] Adding homebrew support

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
     "fmt"
     "os"
+    "path/filepath"
+    "runtime"
 )
+
+// Version is set at build time by goreleaser
+var Version = "snapshot" // Default value for development
 
 func main() {
     if len(os.Args) < 2 {
@@ -16,6 +21,10 @@ func main() {
         portForwardCmd()
     case "connect":
         ssmConnectCmd()
+    case "--version", "-v":
+        binaryName := filepath.Base(os.Args[0])
+        fmt.Printf("%s %s\non %s_%s\n", binaryName, Version, runtime.GOOS, runtime.GOARCH)
+        os.Exit(0)
     default:
         usage()
         os.Exit(1)
@@ -36,6 +45,7 @@ Usage:
 Available Commands:
   port-forward    Establish a port forwarding session to a remote host
   connect         Start an SSM session to a target instance
+  --version, -v   Show version information
 
 Run '%s <command> -h' for more information on a command.
 
